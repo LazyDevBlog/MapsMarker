d82bc152dd527c80cb77d4f695014188ee466568
TBLStudio <iosdev.tbl@gmail.com>
2016-08-12 16:25:12 +0700

    Add Location Manager

--- MapMarkerDemo/MapMarkerDemo/ViewControllers/MainVC.swift
+++ MapMarkerDemo/MapMarkerDemo/ViewControllers/MainVC.swift
@@ -7,31 +7,143 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class MainVC: UIViewController {
+    
+    //MARK: - OUTLET
+    @IBOutlet weak var mapView: MKMapView!
+    
+    //MARK: - Variable
+    var myLocation:CLLocationCoordinate2D?
+    let locationManager = CLLocationManager()
+    
+    //MARK: - Constant
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.navigationController?.navigationBarHidden = true
-
-        // Do any additional setup after loading the view.
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        
+        mapView.delegate = self
+        mapView.mapType = .Standard
+        mapView.zoomEnabled = true
+        mapView.scrollEnabled = true
+        
+        if let coor = mapView.userLocation.location?.coordinate{
+            mapView.setCenterCoordinate(coor, animated: true)
+        }
+       // addLongPressGesture()
+        
+        
+        //Setup bar button items
+        let leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Refresh, target: self, action: #selector(MainVC.leftBarButtonPressed))
+        self.navigationItem.leftBarButtonItem = leftBarButtonItem
+        
+        
+    }
+    
+    //Bar button action
+    static var enable:Bool = true
+    func leftBarButtonPressed () {
+        print("leftBarButtonPressed")
+        if CLLocationManager.locationServicesEnabled() {
+            if MainVC.enable {
+                locationManager.stopUpdatingHeading()
+            }else{
+                locationManager.startUpdatingLocation()
+            }
+            MainVC.enable = !MainVC.enable
+        }
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
+        mapView.showsUserLocation = true;
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        mapView.showsUserLocation = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func centerMap(center:CLLocationCoordinate2D){
+        self.saveCurrentLocation(center)
+        
+        let spanX = 0.007
+        let spanY = 0.007
+        
+        let newRegion = MKCoordinateRegion(center:center , span: MKCoordinateSpanMake(spanX, spanY))
+        mapView.setRegion(newRegion, animated: true)
     }
-    */
+    
+    func saveCurrentLocation(center:CLLocationCoordinate2D){
+        let message = "\(center.latitude) , \(center.longitude)"
+        print(message)
+        //self.lable.text = message
+        myLocation = center
+    }
 
 }
+
+
+extension MainVC: MKMapViewDelegate {
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView?{
+        let identifier = "pin"
+        var view : MKPinAnnotationView
+        if let dequeueView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier) as? MKPinAnnotationView{
+            dequeueView.annotation = annotation
+            view = dequeueView
+        }else{
+            view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            view.canShowCallout = true
+            view.calloutOffset = CGPoint(x: -5, y: 5)
+            view.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure)
+        }
+        view.pinColor =  .Red
+        return view
+    }
+
+}
+
+extension MainVC: CLLocationManagerDelegate {
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        
+        centerMap(locValue)
+    }
+
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
